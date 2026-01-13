3fc0898840e5e2e24badb1a6afe1a0cc3d9dd472
ddbsstar <ddbsstar@example.com>
2026-01-13 23:08:10 +0800

    feat: 灵魂画手用户注册

---
+++ src/users/entity/users.entity.ts
@@ -0,0 +1,36 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('doodler_users')
+export class Users {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 256, nullable: true, comment: '密码' })
+  password: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: false, comment: '昵称' })
+  nickname: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: true, comment: '电子邮箱' })
+  email: string;
+
+  @Column({ type: 'varchar', length: 256, nullable: true, comment: '头像' })
+  avatar: string;
+
+  @Column({
+    type: 'enum',
+    enum: ['female', 'male'],
+    nullable: true,
+    comment: '性别',
+  })
+  gender: string;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+    unique: true,
+    nullable: true,
+    comment: '手机',
+  })
+  phone: string;
+}
